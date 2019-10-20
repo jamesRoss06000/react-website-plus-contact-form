17c1e802c5ecf47efdb2f76169973ded3016c4f7
james ross <james_ross@outlook.fr>
2019-10-20 14:43:07 +0200

    database icons

--- src/components/database.js
+++ src/components/database.js
@@ -1,7 +1,7 @@
 let database = [
   {
     name: 'Trombinoscope',
-    image: 'url(https://i.ibb.co/WsfhjXv/trombinoscope-Thumbnail.png) center / cover',
+    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
     description: 'First group project at Web Dev course. Class photo book created using HTML, CSS and Javascript',
     languages: 'HTML, CSS, JavaScript',
     github: 'https://github.com/jamesRoss06000/trombinoscope',
@@ -9,36 +9,53 @@ let database = [
   },
   {
     name: 'CRUD System',
-    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
-    description: 'Video game database using PHP, SGL, HTML and Boostrap',
+    image: 'url(https://i.ibb.co/PWHn5Mt/phpSql.jpg) center / cover',
+    description: 'Video game database using PHP, SQL, HTML and Boostrap',
     languages: 'PHP, SQL',
     github: 'https://github.com/jamesRoss06000/CRUD',
-    // live: 'https://trombinoscope-group-project.herokuapp.com/'
+    live: 'https://video-game-crud.herokuapp.com/'
   },
   {
+    name: 'Absence Management app',
+    image: 'url(https://i.ibb.co/PWHn5Mt/phpSql.jpg) center / cover',
+    description: 'Partners with Ionic App under Frameworks tab. admin@admin.com, admin',
+    languages: 'PHP, SQL',
+    github: 'https://github.com/jamesRoss06000/Attendance-App',
+    live: 'https://attendance-ics.herokuapp.com/'
+  },
+
+  {
     name: 'Absence Management App',
-    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
+    image: 'url(https://i.ibb.co/wzjCkn3/ionic.png) center / cover',
     description: 'Ionic Angular mobile app that is the user-side of the absence Management CRUD system displayed under the BACK END tab',
     languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/trombinoscope',
-    live: 'https://trombinoscope-group-project.herokuapp.com/'
+    github: 'https://github.com/jamesRoss06000/Attendance-App/tree/master/myApp',
+    // live: ''
   },
   {
     name: 'This Website',
-    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
+    image: 'url(https://i.ibb.co/DYdK9G0/reactjs.png) center / cover',
     description: 'This website was built using ReactJS',
     languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/trombinoscope',
-    live: 'https://trombinoscope-group-project.herokuapp.com/'
+    github: 'https://github.com/jamesRoss06000/react-website',
+    live: 'jamesross.tech/'
   },
   {
     name: 'Tabata',
-    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
+    image: 'url(https://i.ibb.co/wzjCkn3/ionic.png) center / cover',
     description: 'Under construction: <b>Ionic Angular<b/> mobile app for gym fanatics.',
     languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/trombinoscope',
-    live: 'https://trombinoscope-group-project.herokuapp.com/'
-  }
+    github: 'https://github.com/jamesRoss06000/Tabata',
+    // live: ''
+  },
+  {
+    name: 'Online CV Example',
+    image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg) center / cover',
+    description: 'School project: Create an online CV for a classmate',
+    languages: 'HTML, CSS, JavaScript',
+    github: 'https://github.com/jamesRoss06000/Online_CV',
+    live: 'https://cv-example-project.herokuapp.com/'
+  },
 ]
 
 export default database;
