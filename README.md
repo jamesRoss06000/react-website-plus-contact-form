fb922125e17023e61be3da8fbe81d8bc70058ff2
james ross <james_ross@outlook.fr>
2019-11-02 18:50:54 +0100

    go clean

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple one-page website acting as a portfolio.
+
+Components used for each new 'page' and a react-router used to direct the user to option selected.
---
+++ src/components/education.js
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+
+// class Education extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.props.place} {this.props.diploma}</p>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Education;
