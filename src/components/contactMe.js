d794dc34e56d5b19c6bbb38ba88ff936c2c09210
james ross <james_ross@outlook.fr>
2019-11-02 22:02:26 +0100

    pfft

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -66,32 +66,23 @@ class ContactMe extends Component {
         return (
             <div className='contact-body'>
                 <div className='contact-grid'>
-                    {/* <div className='contact-right'> */}
-                    {/* <img src="https://i.ibb.co/3c5K4Cy/MAD-4864.jpg"
-                            alt="James-ROSS" border="0" className='myAvatar' /> */}
                     <h2 id='thanks' style={{ fontSize: '25px' }}>
                         Thanks for visiting...
                         </h2>
-                    <p style={{ width: '75%', margin: 'auto'}}>If you have any questions or know
+                    <p style={{ width: '75%', margin: 'auto' }}>If you have any questions or know
                          of any exciting opportunities you feel I should know about,
                          don't hesitate to contact me. I look forward to hearing from you!
                     </p>
-                    {/* <hr /> */}
                     <h2 id='signature' style={{ fontSize: '25px' }}>
                         James Ross
                         </h2>
-                    {/* </div> */}
-                    {/* <div className='contact-right'> */}
                     <div className='contact-side'>
                         <h2 style={{ fontSize: '25px', fontFamily: 'Anton' }}>
                             Contact Me
                         </h2>
-                        {/* <hr /> */}
                         <div className='contact-list'>
                             <div className='form'>
                                 <div className='alert'>Your message has been sent</div>
-                                {/* ATTEMPT USING PHP */}
-                                {/* <form action="/contact.php" method='post'> */}
                                 <form onSubmit={this.submitHandler} encType='multipart/form-data'>
                                     <input type="text"
                                         name="name" placeholder="Your name..."
@@ -114,29 +105,8 @@ class ContactMe extends Component {
                                     <input type="submit" value="Submit" />
                                 </form>
                             </div>
-                            {/* <List>
-                                    <ListItem>
-                                        <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-                                            <a style={{ color: 'white', textDecoration: 'none' }} href='/'><img src='https://i.ibb.co/jwHSyd0/email-1.png' alt='email' />
-                                                james_ross@outlook.fr</a>
-                                        </ListItemContent>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-                                            <a style={{ color: 'white', textDecoration: 'none' }} href='https://www.linkedin.com/in/jamesross06000/' target='_blank' rel="noopener noreferrer"><img src='https://i.ibb.co/THJNhDp/linkedin.png' alt='email' />
-                                                linkedin.com</a>
-                                        </ListItemContent>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-                                            <a style={{ color: 'white', textDecoration: 'none' }} href='https://github.com/jamesRoss06000' target='_blank' rel="noopener noreferrer"><img src='https://i.ibb.co/hVtB9LR/github-white.png' alt='email' />
-                                                github.com</a>
-                                        </ListItemContent>
-                                    </ListItem>
-                                </List> */}
                         </div>
                     </div>
-                    {/* </div> */}
                 </div>
             </div>
         )
