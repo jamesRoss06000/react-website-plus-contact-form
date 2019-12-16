43760072aefda4530870a915dae3376028c898cf
james ross <james_ross@outlook.fr>
2019-12-16 19:51:25 +0100

    dentist 1 and 2 added

--- src/components/database.js
+++ src/components/database.js
@@ -1,93 +1,108 @@
-let database = [
-  {
-    name: 'Parallax Website',
-    image: 'url(https://i.ibb.co/NxmsXCf/soask.png) center / cover',
-    description: 'Personal project. A mock movie-pitch, read-only website created using HTML and CSS.',
-    languages: 'HTML, CSS, JavaScript',
-    github: 'https://github.com/jamesRoss06000/soask-the-movie',
-    live: 'https://jamesross06000.github.io/soask-the-movie/'
-  },
-  {
-    name: 'Contact Form',
-    image: 'url(https://i.ibb.co/gZj19ZF/330x175contact-Form.png) center / cover',
-    description: 'Example contact form for a non-existing chinese restaurant. HTML, CSS.',
-    languages: 'HTML, CSS, JavaScript',
-    github: 'https://github.com/jamesRoss06000/contact-form',
-    live: 'https://contact-form-html-php.herokuapp.com/'
-  },
-  {
-    name: 'Trombinoscope',
-    image: 'url(https://i.ibb.co/MkNb1FG/330x175trombinoscope.png) center / cover',
-    description: 'Group project. Class "yearbook" created using HTML, CSS and Javascript.',
-    languages: 'HTML, CSS, JavaScript',
-    github: 'https://github.com/jamesRoss06000/trombinoscope',
-    live: 'https://trombinoscope-group-project.herokuapp.com/'
-  },
-  {
-    name: 'CRUD System',
-    image: 'url(https://i.ibb.co/DKpbpv9/330x175-Crudjeux-Video.png) center / cover',
-    description: 'Video game database using PHP, SQL, HTML and styled with Boostrap.',
-    languages: 'PHP, SQL',
-    github: 'https://github.com/jamesRoss06000/CRUD',
-    live: 'https://video-game-crud.herokuapp.com/'
-  },
-  {
-    name: 'Absence Management app',
-    image: 'url(https://i.ibb.co/tLDzdng/330x175-Crud-Abs-App.png) center / cover',
-    description: 'Part 1/2 (see framework tab). PHP, SQL, HTML, Bootstrap. admin@admin.com, admin',
-    languages: 'PHP, SQL',
-    github: 'https://github.com/jamesRoss06000/Attendance-App',
-    live: 'https://attendance-ics.herokuapp.com/'
-  },
+let database = [{
+        name: 'Parallax Website',
+        image: 'url(https://i.ibb.co/NxmsXCf/soask.png) center / cover',
+        description: 'Personal project. A mock movie-pitch, read-only website created using HTML and CSS.',
+        languages: 'HTML, CSS, JavaScript',
+        github: 'https://github.com/jamesRoss06000/soask-the-movie',
+        live: 'https://jamesross06000.github.io/soask-the-movie/'
+    },
+    {
+        name: 'Dentist Website 1',
+        image: 'url(https://i.ibb.co/zszB5z4/dentist1image.png) center / cover',
+        description: 'Website: Angular 8 with Router Module, NGX translate and GoogleMaps',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/dentist',
+        live: 'https://vivid-partition-261120.web.app/'
+    },
+    {
+        name: 'Dentist Website 2',
+        image: 'url(https://i.ibb.co/ZVxpGP8/dentist2image.png) center / cover',
+        description: 'Website: Angular 8 with *ngIf modal selection, NGX translate and GoogleMaps',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/dentist2',
+        live: 'https://dentist-2-67fdf.firebaseapp.com/'
+    },
+    {
+        name: 'Contact Form',
+        image: 'url(https://i.ibb.co/gZj19ZF/330x175contact-Form.png) center / cover',
+        description: 'Example contact form for a non-existing chinese restaurant. HTML, CSS.',
+        languages: 'HTML, CSS, JavaScript',
+        github: 'https://github.com/jamesRoss06000/contact-form',
+        live: 'https://contact-form-html-php.herokuapp.com/'
+    },
+    {
+        name: 'Trombinoscope',
+        image: 'url(https://i.ibb.co/MkNb1FG/330x175trombinoscope.png) center / cover',
+        description: 'Group project. Class "yearbook" created using HTML, CSS and Javascript.',
+        languages: 'HTML, CSS, JavaScript',
+        github: 'https://github.com/jamesRoss06000/trombinoscope',
+        live: 'https://trombinoscope-group-project.herokuapp.com/'
+    },
+    {
+        name: 'CRUD System',
+        image: 'url(https://i.ibb.co/DKpbpv9/330x175-Crudjeux-Video.png) center / cover',
+        description: 'Video game database using PHP, SQL, HTML and styled with Boostrap.',
+        languages: 'PHP, SQL',
+        github: 'https://github.com/jamesRoss06000/CRUD',
+        live: 'https://video-game-crud.herokuapp.com/'
+    },
+    {
+        name: 'Absence Management app',
+        image: 'url(https://i.ibb.co/tLDzdng/330x175-Crud-Abs-App.png) center / cover',
+        description: 'Part 1/2 (see framework tab). PHP, SQL, HTML, Bootstrap. admin@admin.com, admin',
+        languages: 'PHP, SQL',
+        github: 'https://github.com/jamesRoss06000/Attendance-App',
+        live: 'https://attendance-ics.herokuapp.com/'
+    },
 
-  {
-    name: 'Absence Management App',
-    image: 'url(https://i.ibb.co/PWcc1T4/absence.png) center / cover',
-    description: 'Part 2/2 User-side of Absence Management app listed in BACK END tab. Ionic Angular mobile app.',
-    languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/Attendance-App/tree/master/myApp',
-    live: 'https://jamesross06000.github.io/ionic-attendance-app/'
-  },
-  {
-    name: 'This Website',
-    image: 'url(https://i.ibb.co/DYdK9G0/reactjs.png) center / cover',
-    description: 'This website was built using ReactJS. Take a look around, see what you think.',
-    languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/react-website-plus-contact-form',
-    live: 'jamesross.tech/'
-  },
-  {
-    name: 'Instant Message App',
-    image: 'url(https://i.ibb.co/NLts3S7/message-app.png) center / cover',
-    description: 'Simple ReactJS and Firebase instant messaging app.',
-    languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/message-app',
-    live: 'https://jamesross06000.github.io/message-app/'
-  },
-  {
-    name: 'Tabata',
-    image: 'url(https://i.ibb.co/Tt1FYTr/tabata.png) center / cover',
-    description: 'Under construction: Ionic Angular mobile app for gym goers.',
-    languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/Tabata',
-    live: 'https://jamesross06000.github.io/Tabata/home'
-  },
-  {
-    name: 'To-do-list',
-    image: 'url(https://i.ibb.co/DYdK9G0/reactjs.png) center / cover',
-    description: 'Web app built using ReactJS, includes local storage so can used.',
-    languages: 'frameworks',
-    github: 'https://github.com/jamesRoss06000/to-do-list-react-js',
-    live: 'https://jamesross06000.github.io/to-do-list-react-js/'
-  },
-  {
-    name: 'Online CV Example',
-    image: 'url(https://i.ibb.co/0nK4VzR/330x175antoine.png) center / cover',
-    description: 'School project: Create an online CV for a classmate. HTML, CSS, JavaScript',
-    languages: 'HTML, CSS, JavaScript',
-    github: 'https://github.com/jamesRoss06000/Online_CV',
-    live: 'https://cv-example-project.herokuapp.com/'
-  },
+    {
+        name: 'Absence Management App',
+        image: 'url(https://i.ibb.co/PWcc1T4/absence.png) center / cover',
+        description: 'Part 2/2 User-side of Absence Management app listed in BACK END tab. Ionic Angular mobile app.',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/Attendance-App/tree/master/myApp',
+        live: 'https://jamesross06000.github.io/ionic-attendance-app/'
+    },
+    {
+        name: 'This Website',
+        image: 'url(https://i.ibb.co/DYdK9G0/reactjs.png) center / cover',
+        description: 'This website was built using ReactJS. Take a look around, see what you think.',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/react-website-plus-contact-form',
+        live: 'jamesross.tech/'
+    },
+    {
+        name: 'Instant Message App',
+        image: 'url(https://i.ibb.co/NLts3S7/message-app.png) center / cover',
+        description: 'Simple ReactJS and Firebase instant messaging app.',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/message-app',
+        live: 'https://jamesross06000.github.io/message-app/'
+    },
+    {
+        name: 'Tabata',
+        image: 'url(https://i.ibb.co/Tt1FYTr/tabata.png) center / cover',
+        description: 'Under construction: Ionic Angular mobile app for gym goers.',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/Tabata',
+        live: 'https://jamesross06000.github.io/Tabata/home'
+    },
+    {
+        name: 'To-do-list',
+        image: 'url(https://i.ibb.co/DYdK9G0/reactjs.png) center / cover',
+        description: 'Web app built using ReactJS, includes local storage so can used.',
+        languages: 'frameworks',
+        github: 'https://github.com/jamesRoss06000/to-do-list-react-js',
+        live: 'https://jamesross06000.github.io/to-do-list-react-js/'
+    },
+    {
+        name: 'Online CV Example',
+        image: 'url(https://i.ibb.co/0nK4VzR/330x175antoine.png) center / cover',
+        description: 'School project: Create an online CV for a classmate. HTML, CSS, JavaScript',
+        languages: 'HTML, CSS, JavaScript',
+        github: 'https://github.com/jamesRoss06000/Online_CV',
+        live: 'https://cv-example-project.herokuapp.com/'
+    }
 ]
 
 export default database;
