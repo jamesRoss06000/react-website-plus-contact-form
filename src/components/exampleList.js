cbfc023fae657245ab1d5cca87b21f84456244c0
james ross <james_ross@outlook.fr>
2020-01-08 15:55:41 +0100

    example list updated

--- src/components/exampleList.js
+++ src/components/exampleList.js
@@ -1,19 +1,19 @@
 let exampleList = [
   {
     name: 'Dentist Website 2',
-      image: 'url(https://i.ibb.co/ZVxpGP8/dentist2image.png) center / cover',
-      description: 'Website: Angular 8 with *ngIf modal selection, NGX translate and GoogleMaps',
-      languages: 'frameworks',
-      github: 'https://github.com/jamesRoss06000/dentist2',
-      live: 'https://dentist-2-67fdf.firebaseapp.com/'
+    image: 'url(https://i.ibb.co/ZVxpGP8/dentist2image.png) center / cover',
+    description: 'Website: Angular 8 with *ngIf modal selection, NGX translate and GoogleMaps',
+    languages: 'frameworks',
+    github: 'https://github.com/jamesRoss06000/dentist2',
+    live: 'https://dentist-2-67fdf.firebaseapp.com/'
   },
   {
-    name: 'CRUD System',
-    image: 'url(https://i.ibb.co/DKpbpv9/330x175-Crudjeux-Video.png) center / cover',
-    description: 'Video game database using PHP, SQL, HTML and styled with Boostrap.',
-    languages: 'PHP, SQL',
-    github: 'https://github.com/jamesRoss06000/CRUD',
-    live: 'https://video-game-crud.herokuapp.com/'
+    name: 'Share Posts Website',
+    image: 'url(https://i.ibb.co/Mf77mJy/Capture.png) center / cover',
+    description: 'Share posts with other users: PHOP, MVC, OOP Udemy course project',
+    languages: 'HTML, CSS, JavaScript',
+    github: 'https://github.com/jamesRoss06000/share-posts-php-mvc',
+    live: 'https://share-posts-php-mvc.herokuapp.com/'
   },
   {
     name: 'Instant Message App',
